532676db9c2f22a3b6802f60584d1fcac7b22b01
Dazza <adaza90@outlook.com>
2024-10-11 13:23:46 -0500

    Refactor: Move SVG from placeholder slot to PlaceholderImage component
    - Extracted the inline SVG from the `placeholder-img` slot into a new component `PlaceholderImage.vue` to improve code organization and reusability.
    - Replaced the inline SVG with the new `PlaceholderImage` component for better structure and maintainability.

--- src/components/Vue3Dropzone.vue
+++ src/components/Vue3Dropzone.vue
@@ -34,24 +34,7 @@
       <!--   Placeholder content   -->
       <template v-if="!previewUrls.length">
         <slot name="placeholder-img">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="60"
-            height="60"
-            viewBox="0 0 24 24"
-            fill="none"
-            class="transition-all decoration-neutral-150 ease-linear"
-          >
-            <path
-              opacity=".4"
-              d="M22 7.81v6.09l-1.63-1.4c-.78-.67-2.04-.67-2.82 0l-4.16 3.57c-.78.67-2.04.67-2.82 0l-.34-.28c-.71-.62-1.84-.68-2.64-.14l-4.92 3.3-.11.08c-.37-.8-.56-1.75-.56-2.84V7.81C2 4.17 4.17 2 7.81 2h8.38C19.83 2 22 4.17 22 7.81Z"
-              fill="#c3c3c3"
-            ></path>
-            <path
-              d="M9.001 10.381a2.38 2.38 0 1 0 0-4.76 2.38 2.38 0 0 0 0 4.76ZM21.999 13.899v2.29c0 3.64-2.17 5.81-5.81 5.81h-8.38c-2.55 0-4.39-1.07-5.25-2.97l.11-.08 4.92-3.3c.8-.54 1.93-.48 2.64.14l.34.28c.78.67 2.04.67 2.82 0l4.16-3.57c.78-.67 2.04-.67 2.82 0l1.63 1.4Z"
-              fill="#c3c3c3"
-            ></path>
-          </svg>
+          <PlaceholderImage />
         </slot>
         <slot name="title">
           <div class="titles">
@@ -165,6 +148,7 @@
 <script setup>
 import { computed, defineExpose, ref, watchEffect } from "vue";
 import Icon from "./Icon.vue";
+import PlaceholderImage from "./PlaceholderImage.vue";
 
 const props = defineProps({
   modelValue: {
